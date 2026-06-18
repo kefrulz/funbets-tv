e4b38332b4b97f5416a544957dfdb898d99441de
kefrulz <kefrulz@users.noreply.github.com>
2026-06-18 04:41:17 +0200

    Force remote mode in TizenBrew module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function () {
   try {
-    localStorage.setItem("funbetsRemoteMode", "0");
-    document.documentElement.classList.remove("tv-mode");
+    localStorage.setItem("funbetsRemoteMode", "1");
+    document.documentElement.classList.add("tv-mode");
   } catch (error) {}
 }());
